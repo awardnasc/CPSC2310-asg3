0bf3a77f6263f04b041b8f5f63a9b7dde76e210c
Alexander T Ward <atward@koala19.cs.clemson.edu>
2016-10-25 17:19:50 -0400

    "final commit"

--- main.s
+++ main.s
@@ -16,10 +16,48 @@ a	.req r1
 bb	.req r2
 c	.req r3
 
+/*
+ function name: main
+ descriptioin: takes in user input for a, b, and c sends data to be calculated 
+		in mac then prints the received answer.
+ input parameters:
+		a/r1 - holds the value for a input by the user
+		bb/r2 - holds the value for b input by the user called bb because
+			b is already a register
+		c/r3 - holds the value for c input by the user
+ return value:
+		ans/r0 - holds the returned value from mac
+ other output parameters:
+		r0 is used to hold and output print statements
+		r1,r2, and r3 are used to out put the values input by the user
+ effect/output
+		outputs the return value of mac
+ method / effect:
+		uses printf and scanf to prompt and record the user's input for a,
+		b, and c	
+ typical calling sequence:
+		read all of the user input sequentially onto r1 incrementing the 
+			stackpointer by 4.
+		Print the "a + (b * c) =" part
+		move a, bb, and c to r0-r2		
+		load r0-r2 and send to mac
+		receive the answer from mac in ans and move that to r1 for 
+			printing
+		print the answer			
+
+ local register usage:
+		r0 is used to hold all of the print statements and read formats
+		a, bb, c hold the corresponding values from the user.
+*/
+
 main:
 	push {c, lr}
+	
+	@make room for 3 integers	
 	add sp, sp, #-12
 	
+
+	@prompt the user and read in 3 integers
 	ldr r0, =prt_a
 	bl printf
 	ldr r0, =rdfmt	
@@ -38,6 +76,8 @@ main:
 	add r1, sp, #8
 	bl scanf
 
+	
+	@put the user input into a, bb, and c
 	add r0, sp, #0
 	ldr a, [r0]
 	add r0, sp, #4 	
@@ -45,13 +85,21 @@ main:
 	add r0, sp, #8
 	ldr c, [r0]
 
+	@ print "a + (b * c) =
 	ldr r0, =prt_fmt1
 	bl printf
 	
-        ldmia sp, {a-c}
+	@prepare a, b, and c to be sent in r0, r1, and r2
+	mov r0, a
+	mov r1, bb
+	mov r2, c
+
+	@load r0-r2 and send them to mac
+        ldmia sp, {r0-r2}
         bl mac
 	
-	mov r1, ans 	@put answer in r1
+        @put answer in r1 and print it
+	mov r1, ans 	
 	ldr r0, =prt_fmt2
 	bl printf
 
--- prog3.s
+++ prog3.s
@@ -12,25 +12,52 @@
 	.global mac
 	.type mac, %function
 
-ans	.req r0
-a	.req r1
-bb	.req r2
-c	.req r3
+a	.req r0
+bb	.req r1
+c	.req r2
 
+/*
+ function name: mac
+ descriptioin: takes in user input for a, b, and c from main it then multiplies 
+		b and c then adds the answer to a to get the final answer
+ input parameters:
+		a/r1 - holds the value for a input by the user
+		bb/r2 - holds the value for b input by the user called bb because
+			b is already a register
+		c/r3 - holds the value for c input by the user
+ return value:
+		ans/r0 - holds the final value from the calculation
+ other output parameters:
+		none
+ effect/output
+		Sends the answer in ans back to main
+ method / effect:
+		multiply b and c,  add that number to a then return that number to
+		main in ans 	
+ typical calling sequence:
+		push lr
+		multiply c by bb and set that to bb
+		add the new bb to a saving that in a/r0
+		pop lr			
+
+ local register usage:
+		a - holds the value for a and then the final answer
+		bb - holds b and then b * c
+		c - holds c
+*/
 
 mac: 
-
-	push {c, lr}
-
+        
+	push {lr}
+	
+	@ a + (b * c) = a
 	mul bb, c, bb
 	add a, a, bb
-	mov ans, a
-
+	
 done: 
 
-	pop {c, pc}
+	pop {pc}
 
-.unreq ans
 .unreq a
 .unreq bb
 .unreq c
